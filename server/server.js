cef85f36f21ba327755b9c03a3e2abc4a091d204
DeepakBarwal <DeepakBarwal98@gmail.com>
2022-12-27 12:54:39 +0530

    connected to mongodb atlas via mongoose, congigured dotenv and changed dev to serve in package.json

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
+
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('DB connection successful');
+    } catch (error) {
+        console.log('DB connection unsuccessful');
+        console.error(`Error: ${error.message}`);
+    }
+}
+connectDb();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
