8be320ec7b3e6275101556e90a979751050a6d47
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-01-02 12:19:34 +0530

    server: installed bcrypt, storing the hashed password in db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,25 +1,31 @@
 import {Router} from 'express';
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 const router = Router();
 
 router.post('/register', async (req, res) => {
     // get all form data
     const {firstName, lastName, email, password} = req.body;
+
     // check if user exists with same email
     try {
         const userExists = await User.findOne({email});
         if (userExists) {
             return res.status(409).json({message: 'User already exists.'});
         }
-        const user = await User({firstName, lastName, email, password});
+        // hash the password
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = await User({firstName, lastName, email, password: hashedPassword});
+        // store user in db
         const savedUser = await user.save();
-        res.json({message: 'user is created', user: savedUser});
+        res.status(201).json({message: 'user is created', user: savedUser});
     } catch (error) {
         console.error(error.message);
     }
-    // hash the password
-    // store user in db
 });
 
 export default router;
