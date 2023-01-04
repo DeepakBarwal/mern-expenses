df60a83b8323ebc368fa1882a9a93534e15c92d2
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-01-04 10:57:34 +0530

    client: instead of checking only the token cookie, gearing up to fetch the user from backend using token

--- client/src/utils/CheckAuth.js
+++ client/src/utils/CheckAuth.js
@@ -1,8 +1,19 @@
 import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const CheckAuth = ({children}) => {
     const token = Cookies.get('token');
+    const fetchUser = async () => {
+        await fetch(`${process.env.REACT_APP_API_URL}/user`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    };
+    useEffect(() => {
+        fetchUser();
+    }, []);
     return (
         token ? children : <Navigate to='/login' replace={true} />
     );
