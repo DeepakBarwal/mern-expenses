75f53c8e8f5f7cbd1e1a8c9d7e9273d5cd9703ed
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-01-04 12:05:52 +0530

    client: CheckAuth - fetch user must succeed in order to view /, otherwise redirect to /login

--- client/src/utils/CheckAuth.js
+++ client/src/utils/CheckAuth.js
@@ -1,22 +1,36 @@
 import Cookies from 'js-cookie';
-import { useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const CheckAuth = ({children}) => {
     const token = Cookies.get('token');
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+
     const fetchUser = async () => {
-        await fetch(`${process.env.REACT_APP_API_URL}/user`, {
-            headers: {
-                Authorization: `Bearer ${token}`
+        setIsLoading(true);
+        try {
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/user`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setIsLoading(false);
+            if (!res.ok) {
+                return navigate('/login');
             }
-        });
+        } catch (error) {
+            console.error(error.message);
+        }
     };
     useEffect(() => {
         fetchUser();
     }, []);
-    return (
-        token ? children : <Navigate to='/login' replace={true} />
-    );
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    return children;
 };
 
 export default CheckAuth;
