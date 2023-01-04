1fe4410102e23239fa8966270ae6cf72be275ce2
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-01-04 12:26:18 +0530

    client: introduced redux store to our app and created mock auth slice

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import ButtonAppBar from './components/AppBar';
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getUser } from './store/auth';
 
 function App() {
+  const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  console.log(auth);
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
   return (
     <div>
       <ButtonAppBar />
